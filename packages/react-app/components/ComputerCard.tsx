a37be809435f58e15d21ae47b58822da6ef52f8a
dennohkim <kimathidennis38@gmail.com>
2023-04-25 20:53:25 +0300

    feat: list computers

--- packages/react-app/components/ComputerCard.tsx
+++ packages/react-app/components/ComputerCard.tsx
@@ -17,10 +17,10 @@ const ComputerCard = ({computer}: {computer: Computer}) => {
  
   return (
     <div className="group flex flex-col space-y-6">
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+      <div className="aspect-square w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
         <Image
-          width={300}
-          height={300}
+          width="300"
+          height="300"
           src={computer.image_url}
           alt="computer"
           className="object-center group-hover:opacity-75 object-cover transform transition-all h-full w-full hover:scale-110 "
